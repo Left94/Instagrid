6ae1cad008ae84bf3d7a5d2d1c4a459455c2419c
Left94 <leftheri94@gmail.com>
2018-03-01 13:28:20 +0100

    created custom view class MainView class

---
+++ Instagrid/View/MainView.swift
@@ -0,0 +1,26 @@
+//
+//  MainView.swift
+//  Instagrid
+//
+//  Created by vincent  on 01/03/2018.
+//  Copyright © 2018 Loret Vincent. All rights reserved.
+//
+
+import UIKit
+
+class MainView: UIView {
+
+    @IBOutlet var roundedView1Image : UIImageView?
+    @IBOutlet var roundedView2Image : UIImageView?
+    @IBOutlet var roundedView3Image : UIImageView?
+    @IBOutlet var roundedView4Image : UIImageView?
+    @IBOutlet var roundedView5Image : UIImageView?
+    @IBOutlet var roundedView6Image : UIImageView?
+
+    @IBOutlet var roundedView1Button : UIButton?
+    @IBOutlet var roundedView2Button : UIButton?
+    @IBOutlet var roundedView3Button : UIButton?
+    @IBOutlet var roundedView4Button : UIButton?
+    @IBOutlet var roundedView5Button : UIButton?
+    @IBOutlet var roundedView6Button : UIButton?
+}
